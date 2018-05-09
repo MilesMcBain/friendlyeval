e1058cae6f54bc3289b14ead3ac96a3a7bb717e5
milesmcbain <miles.mcbain@gmail.com>
2018-05-09 22:06:44 +1000

    Add documentation for typed_as_name, and typed_as_name_lhs.

--- R/rlung.R
+++ R/rlung.R
@@ -1,22 +1,41 @@
-##' Take what was typed and use it as a column name.
+##' Take what was typed and use it as a column name argument in a dplyr
+##' function.
 ##'
-##' This is used inside a function to pass the literal text of what the user typed as an argument to another function. When using dplyr the text will typically be destined will typically be destined for a column name argument. See the select example which passes the literal text given for `a_col` - "cyl" as the argument to `select`.
+##' This is used inside a function to pass the literal text of what the user
+##' typed as an argument to another function. When using dplyr the text will
+##' typically be destined for a column name argument. See examples for usage
+##' scenarios.
 ##' 
-##' @title 
-##' @param a_value 
-##' @return 
+##' @title typed_as_name
+##' @param a_name the argument for which the user typed text that is to be used
+##'   as a column name.
+##' @return Something that will resolve to a column named when prefixed with !!.
 ##' @examples
 ##' select_this <- function(a_col){
 ##'  select(mtcars, !!typed_as_name(a_col))
 ##' }
 ##' select_this(cyl)
-##' @export
-typed_as_name_rhs <- rlang::enquo
-
-
-##' .. content for \description{} (no empty lines) ..
 ##'
-##' .. content for \details{} ..
+##' mutate_this <- function(a_col){
+##'  mutate(mtcars, result = mean(!!typed_as_name(a_col)))
+##' }
+##' mutate_this(cyl)
+##' 
+##' @export
+typed_as_name <- rlang::enquo
+
+
+##' Take what was typed and use it as a column name argument in dplyr on the
+##' left hand side of an equality. 
+##'
+##' This is used inside a function to pass the literal text of what the user
+##' typed as a function argument to another function. This function applies in
+##' the special case that the text is intended to be used on the left hand side
+##' of an an equality. For example to replace `my_col` in `mutate(my_col = pi)`.
+##' In this case the call must be rewritten as
+##' `mutate(!!typed_as_name_lhs(my_col) := pi)`. Note the usage of `:=`. This is
+##' an additional requirement when using !! on the left hand side of an
+##' equality.
 ##' @title 
 ##' @param arg 
 ##' @return 
@@ -36,6 +55,15 @@ typed_as_name_lhs <- function(arg){
   quo_name(eval.parent(enquo(arg)))
 }
 
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title  
+##' @param a_value 
+##' @return
+##' @examples
+##' @export
 typed_list_as_name_list <- rlang::enquos
 
 ##' .. content for \description{} (no empty lines) ..
@@ -51,4 +79,12 @@ typed_list_as_name_list <- rlang::enquos
 ##' @export
 value_as_name <- rlang::sym
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title  
+##' @param a_value 
+##' @return
+##' @examples
+##' @export
 value_list_as_name_list <- rlang::syms
